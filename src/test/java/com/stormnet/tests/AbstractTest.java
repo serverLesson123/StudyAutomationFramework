09689cdbe8c21f71d431a07dc4b1f2ef938a55e8
Pavel.Chachotkin <Agentbars00070>
2021-10-27 20:16:04 +0300

    Add screenshot

--- src/test/java/com/stormnet/tests/AbstractTest.java
+++ src/test/java/com/stormnet/tests/AbstractTest.java
@@ -1,12 +1,21 @@
 package com.stormnet.tests;
 
+import com.stormnet.listeners.CustomITestNgListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import io.qameta.allure.testng.AllureTestNg;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+import java.io.ByteArrayInputStream;
 
+@Listeners({CustomITestNgListener.class, AllureTestNg.class})
 public class AbstractTest {
 	protected WebDriver driver;
 
@@ -18,10 +27,16 @@ public class AbstractTest {
 		this.driver = driver;
 	}
 
-
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		if (!result.isSuccess()) {
+			Allure.attachment("attachment2.png", new ByteArrayInputStream(takeScreenshot()));
+		}
 		driver.close();
+	}
+
+	public byte[] takeScreenshot() {
+		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 	}
 
 }
